a7303d8608343a4bf0fcb59fc99ce4bd089f15cf
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2024-01-06 17:50:50 -0500

    landing page first draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,41 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-[1440px] h-[1362px] relative bg-stone-50">
+    <div className="w-[1062px] h-[783px] left-[314px] top-[119px] absolute">
+        <img className="w-[249px] h-[249px] left-0 top-0 absolute" src="https://via.placeholder.com/249x249" />
+        <div className="w-[249px] h-[249px] left-0 top-[534px] absolute bg-rose-100" />
+        <img className="w-[249px] h-[249px] left-[813px] top-0 absolute" src="https://via.placeholder.com/249x249" />
+        <img className="w-[249px] h-[249px] left-[270px] top-0 absolute" src="https://via.placeholder.com/249x249" />
+        <div className="w-[249px] h-[249px] left-[270px] top-[534px] absolute bg-rose-100" />
+        <img className="w-[249px] h-[249px] left-[270px] top-[267px] absolute" src="https://via.placeholder.com/249x249" />
+        <img className="w-[249px] h-[249px] left-0 top-[267px] absolute" src="https://via.placeholder.com/249x249" />
+        <div className="w-[249px] h-[249px] left-[541px] top-[534px] absolute bg-rose-100" />
+        <img className="w-[249px] h-[249px] left-[541px] top-0 absolute" src="https://via.placeholder.com/249x249" />
+        <div className="w-[249px] h-[249px] left-[811px] top-[534px] absolute bg-rose-100" />
+        <div className="w-[249px] h-[249px] left-[811px] top-[267px] absolute bg-rose-100" />
+    </div>
+    <div className="w-[1440px] h-40 left-0 top-[1202px] absolute bg-rose-100" />
+    <div className="left-[47px] top-[1185px] absolute text-green-600 text-[40px] font-normal font-['Tomarik'] uppercase leading-[25px]">Let’s connect!</div>
+    <div className="left-[50px] top-[1245px] absolute text-lime-900 text-base font-normal font-['Corbel'] leading-[23px] tracking-wide">Email me at: yasuda.i@ufl.edu<br/>Text me at: (561)339-0255<br/>My LinkedIn: https://www.linkedin.com/in/izumi-yasuda/</div>
+    <div className="w-10 h-[25px] left-[41px] top-[168px] absolute">
+        <div className="w-10 h-3 left-0 top-[10px] absolute bg-rose-100" />
+        <div className="left-[3px] top-0 absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">All</div>
+    </div>
+    <div className="left-[44px] top-[256px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">print</div>
+    <div className="left-[44px] top-[300px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">motion</div>
+    <div className="left-[44px] top-[344px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">marketing</div>
+    <div className="left-[44px] top-[388px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">illustration</div>
+    <div className="left-[44px] top-[212px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">branding</div>
+    <div className="w-[1440px] h-[113px] left-0 top-0 absolute">
+        <div className="w-[1440px] h-[82px] left-0 top-0 absolute bg-stone-50" />
+        <img className="w-[153px] h-[81px] left-[47px] top-[32px] absolute" src="https://via.placeholder.com/153x81" />
+        <div className="left-[1314px] top-[32px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">contact</div>
+        <div className="w-[60px] h-[25px] left-[1123px] top-[32px] absolute">
+            <div className="w-[60px] h-3 left-0 top-[10px] absolute bg-rose-100" />
+            <div className="left-[3px] top-0 absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">Work</div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="left-[1217px] top-[32px] absolute text-lime-900 text-base font-normal font-['Corbel'] uppercase leading-[25px] tracking-[2.88px]">about</div>
+    </div>
+</div>
   )
 }
