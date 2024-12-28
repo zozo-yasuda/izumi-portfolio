3f3adbf0e7a9fd176713d029fa8c9153dfe4f484
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2024-12-28 17:17:27 -0500

    fixed footer issue

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,14 +1,19 @@
 "use client";
-import { usePathname } from 'next/navigation'
-import FooterText from './footerText';
+import { usePathname } from "next/navigation";
+import FooterText from "./footerText";
 
-const Footer = ()=>
-{
-    const exceptions = ['/contact', '/fauxflorida'];
-    return (
-      <div className="pt-6 bg-offwhite">
-        {!exceptions.includes(usePathname()) && <FooterText />}
-      </div>
-    );
-}
- export default Footer;
+const Footer = () => {
+  const exceptions = [
+    "/contact",
+    "/fauxflorida",
+    "/example",
+    "/dotndone",
+    "/fetzottoswimwear",
+  ];
+  return (
+    <div className="pt-6 bg-offwhite">
+      {!exceptions.includes(usePathname()) && <FooterText />}
+    </div>
+  );
+};
+export default Footer;
