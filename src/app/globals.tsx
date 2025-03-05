2b9634b7a45d9b42156ccc602e0150dede34402b
zoomi4649 <132007724+zoomi4649@users.noreply.github.com>
2025-03-04 22:43:31 -0500

    Update globals.tsx

--- src/app/globals.tsx
+++ src/app/globals.tsx
@@ -25,9 +25,9 @@ export const FAUX_FLORIDA = new Artwork(
   "/fauxflorida",
   "FAUX FLORIDA",
   "Project",
-  "/thumbnails/tn1.png",
+  "/thumbnails/CloverValley.png",
   [TAG.PRINT],
-  ""
+  "/pdf/CloverValley.pdf"
 );
 
 export const DOTNDONE = new Artwork(
@@ -54,48 +54,42 @@ export const SOLUNA = new Artwork(
   "/soluna",
   "SOLUNA",
   "Project",
-  "/thumbnails/tn2.png",
+  "/thumbnails/Soluna.png",
   [TAG.BRANDING],
-  ""
+  "/pdf/Soluna.pdf"
 );
 
 export const KISUI_TECH_FLYER = new Artwork(
   "/kisuitechflyer",
   "KISUI TECH FLYER",
   "Project",
-  "/thumbnails/tn3.png",
+  "/thumbnails/KisuiTech.png",
   [TAG.PRINT],
-  ""
+  "/pdf/KisuiTech.pdf"
 );
-export const AKM_30 = new Artwork(
-  "/akm30",
-  "AKM 30",
-  "Project",
-  "/thumbnails/tn4.png",
-  [TAG.PRINT, TAG.BRANDING],
-  ""
-);
+
 export const MUSIC_THEORY_CAT = new Artwork(
   "/musictheorycat",
   "MUSIC THEORY CAT",
   "Project",
-  "/thumbnails/tn5.png",
+  "/thumbnails/Remi.png",
   [TAG.ILLUSTRATION],
-  ""
+  "/pdf/Remi.pdf"
 );
 export const DESIGN_HEROS = new Artwork(
   "/designheros",
   "DESIGN HEROS",
   "Project",
-  "/thumbnails/tn6.png",
+  "/thumbnails/CarolynHawkins.png",
   [TAG.MOTION],
-  ""
+  "/pdf/CarolynHawkins.pdf"
 );
+
 export const FETZ_OTTO_SWIMWEAR = new Artwork(
   "/fetzotto",
   "FETZ OTTO",
   "Swimwear",
-  "/thumbnails/tn7.png",
+  "/thumbnails/FetzOtto.png",
   [TAG.BRANDING],
   "/pdfs/FetzOtto.pdf",
   (
@@ -111,49 +105,53 @@ export const FETZ_OTTO_SWIMWEAR = new Artwork(
     </p>
   )
 );
-export const THE_RABBIT_HOLE = new Artwork(
-  "/therabbithole",
-  "THE RABBIT HOLE",
-  "Project",
-  "/thumbnails/tn8.png",
-  [TAG.MOTION, TAG.BRANDING, TAG.PRINT],
-  "/pdfs/FetzOtto (1).pdf"
+
+export const RAW_EDGE = new Artwork(
+  "/rawedge",
+  "RAW EDGE",
+  "Mending Workshop",
+  "/thumbnails/RawEdge.png",
+  [TAG.MOTION],
+  "/pdf/RawEdge.pdf"
 );
-export const FETZ_OTTO_DAYCARE = new Artwork(
-  "/fetzottodaycare",
-  "FETZ OTTO DAYCARE",
-  "Project",
-  "/thumbnails/tn9.png",
-  [],
-  ""
+
+export const MINDFULL = new Artwork(
+  "/mindfull",
+  "MINDFULL",
+  "Game Concept",
+  "/thumbnails/Mindfull.png",
+  [TAG.MOTION],
+  "/pdf/Mindfull.pdf"
 );
-export const SWEET_SKETCH = new Artwork(
-  "/sweetsketch",
-  "SWEET SKETCH",
-  "Project",
-  "/thumbnails/tn10.png",
-  [],
-  ""
+
+export const NAVIREADER = new Artwork(
+  "/navireader",
+  "NAVIREADER",
+  "Reading Tool",
+  "/thumbnails/NaviReader.png",
+  [TAG.MOTION],
+  "/pdf/NaviReader.pdf"
 );
-export const IZUMI_YASUDA = new Artwork(
-  "/izumiyasuda",
-  "IZUMI YASUDA",
-  "Project",
-  "/thumbnails/tn11.png",
-  [],
+
+export const ELECTROPHYSICS = new Artwork(
+  "/electrophysics",
+  "ELECTROPHYSICS",
+  "Motion Graphic",
+  "/thumbnails/Electrophysics.png",
+  [TAG.MOTION],
   ""
 );
 
 export const artworks: Array<Artwork> = [
-  AKM_30,
   DESIGN_HEROS,
   FAUX_FLORIDA,
-  FETZ_OTTO_DAYCARE,
   FETZ_OTTO_SWIMWEAR,
-  IZUMI_YASUDA,
   KISUI_TECH_FLYER,
   MUSIC_THEORY_CAT,
   SOLUNA,
-  SWEET_SKETCH,
   DOTNDONE,
+  RAW_EDGE,
+  MINDFULL,
+  NAVIREADER,
+  ELECTROPHYSICS,
 ];
