f5a115da61b90ead9a6e7ab6d4039eb8a5a92e9c
zoomi4649 <132007724+zoomi4649@users.noreply.github.com>
2025-03-05 14:05:47 -0500

    Create page.tsx

---
+++ src/app/electrophysics/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Sidebar from "../components/artsidebar";
+import PDFPage from "../components/portfolioPages/pdfPage";
+import { ELECTROPHYSICS } from "../globals";
+
+export default function ElectrophysicsPage() {
+  return (
+    <PDFPage
+      sidebar={Sidebar}
+      artwork={ELECTROPHYSICS}
+      fileName={"/pdfs/NaviReader.pdf"}
+    />
+  )
+}
