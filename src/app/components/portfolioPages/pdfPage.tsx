3b0e5147794921741da3e317525683a21f86e092
nzmysd <nozomi.yasuda.4649@gmail.com>
2025-03-07 16:33:50 -0500

    displaying multiple pages

--- src/app/components/portfolioPages/pdfPage.tsx
+++ src/app/components/portfolioPages/pdfPage.tsx
@@ -4,17 +4,14 @@ import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 
 const Document = dynamic(
-  () => import("react-pdf").then((module) => (module.Document)),
+  () => import("react-pdf").then((module) => module.Document),
   {
     ssr: false,
   }
 );
-const Page = dynamic(
-  () => import("react-pdf").then((module) => (module.Page)),
-  {
-    ssr: false,
-  }
-);
+const Page = dynamic(() => import("react-pdf").then((module) => module.Page), {
+  ssr: false,
+});
 
 interface SideBarProps {
   artwork: Artwork;
@@ -29,31 +26,35 @@ interface PDFPageProps {
   mindfullVideo2?: string;
 }
 
-const PDFPage: React.FC<PDFPageProps> = ({ sidebar, fileName, artwork, video, mindfullVideo1, mindfullVideo2 }) => {
-  
-
+const PDFPage: React.FC<PDFPageProps> = ({
+  sidebar,
+  fileName,
+  artwork,
+  video,
+  mindfullVideo1,
+  mindfullVideo2,
+}) => {
   useEffect(() => {
     import("react-pdf").then(({ pdfjs }) => {
       pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
     });
   }, []);
-  
 
   const SidebarComponent = sidebar;
 
   const [pageWidth, setPageWidth] = useState(0);
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       setPageWidth(window.innerWidth);
 
       const handleResize = () => {
-          setPageWidth(window.innerWidth);
+        setPageWidth(window.innerWidth);
       };
 
-      window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
 
       return () => {
-          window.removeEventListener('resize', handleResize);
+        window.removeEventListener("resize", handleResize);
       };
     }
   }, []);
@@ -62,29 +63,45 @@ const PDFPage: React.FC<PDFPageProps> = ({ sidebar, fileName, artwork, video, mi
     videoFile: string;
   }
 
-  const Video: React.FC<VideoProps> = ({videoFile}) => {
+  const Video: React.FC<VideoProps> = ({ videoFile }) => {
     return (
-      <video width={Math.floor(2/3 * pageWidth) } height="240" controls preload="none">
+      <video
+        width={Math.floor((2 / 3) * pageWidth)}
+        height="240"
+        controls
+        preload="none"
+      >
         <source src={videoFile} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-    )
-  }
+    );
+  };
+
+  const [numPages, setNumPages] = useState(0);
 
   return (
     <section className="bg-offwhite flex flex-col">
       <div className="grid grid-cols-3">
         <SidebarComponent artwork={artwork} />
         <div className="col-span-2 flex flex-col">
-          {video?(<Video videoFile={video}></Video>):(<></>)}
-          <Document file={fileName}>
-            <Page pageNumber={1} renderAnnotationLayer={false} renderTextLayer={false} width={ Math.floor(2/3 * pageWidth)} />
+          {video ? <Video videoFile={video}></Video> : <></>}
+          <Document file={fileName} onLoadSuccess={({ numPages }) => {
+            setNumPages(numPages);
+          }}>
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+                width={Math.floor((2 / 3) * pageWidth)}
+              />
+            ))}
           </Document>
-          {mindfullVideo1?(<Video videoFile={mindfullVideo1}></Video>):(<></>)}
-          {mindfullVideo2?(<Video videoFile={mindfullVideo2}></Video>):(<></>)}
-          </div>
+          {mindfullVideo1 ? <Video videoFile={mindfullVideo1}></Video> : <></>}
+          {mindfullVideo2 ? <Video videoFile={mindfullVideo2}></Video> : <></>}
+        </div>
       </div>
-      
     </section>
   );
 };
