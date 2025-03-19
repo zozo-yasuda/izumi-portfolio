e2fbe917c4007f0a3747184169847f3108eddc54
nzmysd <nozomi.yasuda.4649@gmail.com>
2025-03-19 18:30:38 -0400

    adjusting page margins

--- src/app/components/artsidebar/index.tsx
+++ src/app/components/artsidebar/index.tsx
@@ -4,61 +4,94 @@ import { artworks } from "../../globals";
 import Link from "next/link";
 
 interface SideBarProps {
-  artwork : Artwork;
+  artwork: Artwork;
   styles?: string;
 }
 
-const Sidebar:React.FC<SideBarProps> = ({artwork, styles}) => {
-  const Tags = () => Array.from(artwork.tags).map((tag, index) => {
-    return (
-    <li key={index} className="flex bg-rose rounded-full px-5 py-2">
-      {tag}
-    </li>);
-  });
-  const currIndex =  artworks.indexOf(artwork);
-    const prevArt:Artwork|null = currIndex > 0? artworks[currIndex-1]: null;
-    const nextArt:Artwork|null = currIndex < artworks.length-1? artworks[currIndex+1]: null;
-  
-    const prevButton = () => {
-      if (prevArt != null){
-        return (
-          <Link href={prevArt.key} className="flex items-center px-5 py-2 bg-lime text-white text-sm rounded-full">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="size-4 pr-1">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-        </svg>
-          {prevArt.title}</Link>
-        );
-      }
+const Sidebar: React.FC<SideBarProps> = ({ artwork, styles }) => {
+  const Tags = () =>
+    Array.from(artwork.tags).map((tag, index) => {
+      return (
+        <li key={index} className="flex bg-rose rounded-full px-3 sm:px-5 py-1 sm:py-2">
+          {tag}
+        </li>
+      );
+    });
+  const currIndex = artworks.indexOf(artwork);
+  const prevArt: Artwork | null =
+    currIndex > 0 ? artworks[currIndex - 1] : null;
+  const nextArt: Artwork | null =
+    currIndex < artworks.length - 1 ? artworks[currIndex + 1] : null;
+
+  const prevButton = () => {
+    if (prevArt != null) {
+      return (
+        <Link
+          href={prevArt.key}
+          className="flex items-center px-5 py-2 bg-lime text-white text-sm rounded-full"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="3"
+            stroke="currentColor"
+            className="size-4 pr-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+          {prevArt.title}
+        </Link>
+      );
     }
-    const nextButton = () => {
-      if (nextArt != null){
-        return (
-          <Link href={nextArt.key} className="flex items-center px-5 py-2 bg-lime text-white text-sm rounded-full" >{nextArt.title}  
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="size-4 pl-1">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-        </svg>
-          </Link>
-        );
-      }
+  };
+  const nextButton = () => {
+    if (nextArt != null) {
+      return (
+        <Link
+          href={nextArt.key}
+          className="flex items-center px-5 py-2 bg-lime text-white text-sm rounded-full"
+        >
+          {nextArt.title}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="3"
+            stroke="currentColor"
+            className="size-4 pl-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </Link>
+      );
     }
+  };
   return (
     <aside className={styles}>
       <Navigation />
-      <div className="sm:pt-6">
-        </div>
-      <div className="px-20 pt-2 text-lime uppercase font-tomarik text-4xl md:text-5xl lg:text-6xl">
+      <div className="pb-2"></div>
+      <div className="px-10 sm:px-20 pt-2 text-lime uppercase font-tomarik text-4xl md:text-5xl lg:text-6xl">
         {artwork.title}
       </div>
-      <div className="px-20 tracking-24 font-tomarik text-offblack text-xl md:text-lg lg:text-xl">
+      <div className="px-10 sm:px-20 tracking-24 font-tomarik text-offblack text-xl md:text-lg lg:text-xl">
         {artwork.subtitle}
       </div>
-      <ul className="pt-4 px-20 py-2 uppercase text-bold text-green tracking-18 font-sans flex flex-row flex-wrap gap-x-2 gap-y-3 text-sm">
+      <ul className="pt-4 px-10 sm:px-20 py-2 uppercase text-bold text-green tracking-18 font-sans flex flex-row flex-wrap gap-x-2 gap-y-3 text-sm">
         {Tags()}
       </ul>
-      <div className="px-20 text-left pt-4 text-offblack font-sans tracking-5 text-sm text-pretty">
+      <div className="px-10 sm:px-20 text-left pt-4 text-offblack font-sans tracking-5 text-sm text-pretty">
         {artwork.text}
       </div>
-      <div className="flex justify-between py-16 px-20 text-left pt-8 text-green font-sans tracking-5 text-sm">
+      <div className="flex justify-between py-16 px-10 sm:px-20 text-left pt-8 text-green font-sans tracking-5 text-sm">
         {prevButton()}
         {nextButton()}
       </div>
--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -19,66 +19,64 @@ const Navbar = () => {
     );
   };
 
-    const [pageWidth, setPageWidth] = useState(0);
-    useEffect(() => {
-      if (typeof window !== "undefined") {
-        setPageWidth(window.innerWidth);
-  
-        const handleResize = () => {
-          setPageWidth(window.innerWidth);
-        };
-  
-        window.addEventListener("resize", handleResize);
-  
-        return () => {
-          window.removeEventListener("resize", handleResize);
-        };
-      }
-    }, []);
-  
-    const [isMobile, setIsMobile] = useState(false);
-  
-    useEffect(() => {
+  const [pageWidth, setPageWidth] = useState(0);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setPageWidth(window.innerWidth);
+
       const handleResize = () => {
-        setIsMobile(window.innerWidth <= 768);
+        setPageWidth(window.innerWidth);
       };
-  
-      if (typeof window !== "undefined") {
-        setIsMobile(window.innerWidth <= 768);
-        window.addEventListener("resize", handleResize);
-        return () => {
-          window.removeEventListener("resize", handleResize);
-        };
-      }
-    }, []);
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    if (typeof window !== "undefined") {
+      setIsMobile(window.innerWidth <= 768);
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
 
   return (
-    <nav className="sticky bg-white flex col-span-1 z-50 top-0 p-5">
+    <nav className="sticky bg-white flex sm:col-span-1 z-50 top-0">
       <>
-        <div className="flex flex-col justify-between">
-          <div className={isMobile?"pl-10 mx-auto":"pl-10"}>
-            <ul className=" text-xs flex flex-row items-center gap-x-3">
-              <li className="p-2 pl-5">
-                <Logo />
-              </li>
-              <li>
-                <Link
-                  href={"/"}
-                  className="relative font-bold z-10 py-2 px-7 rounded-full block text-offblack bg-transparent font-sans uppercase tracking-24 border-2 border-offblack hover:text-white hover:border-transparent hover:bg-offblack"
-                >
-                  work
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/about"}
-                  className="relative font-bold z-10 py-2 px-4 rounded-full block text-offblack bg-transparent font-sans uppercase tracking-24 border-2 border-offblack hover:text-white hover:border-transparent hover:bg-offblack"
-                >
-                  about me
-                </Link>
-              </li>
-            </ul>
-          </div>
+        <div className={isMobile ? "p-5" : "pl-20 py-5"}>
+          <ul className="text-xs flex flex-row items-center gap-x-2 sm:gap-x-3 ">
+            <li className="p-2 pl-5">
+              <Logo />
+            </li>
+            <li>
+              <Link
+                href={"/"}
+                className="relative font-bold z-10 py-2 px-7 rounded-full block text-offblack bg-transparent font-sans uppercase tracking-24 border-2 border-offblack hover:text-white hover:border-transparent hover:bg-offblack"
+              >
+                work
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/about"}
+                className="relative font-bold z-10 py-2 px-4 rounded-full block text-offblack bg-transparent font-sans uppercase tracking-24 border-2 border-offblack hover:text-white hover:border-transparent hover:bg-offblack"
+              >
+                about me
+              </Link>
+            </li>
+          </ul>
         </div>
       </>
     </nav>
