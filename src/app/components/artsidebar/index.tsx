80dd4dc4baad9608041c7f1b9f3a0e169abd5bdc
nzmysd <nozomi.yasuda.4649@gmail.com>
2025-03-19 17:34:13 -0400

    font size adjustment

--- src/app/components/artsidebar/index.tsx
+++ src/app/components/artsidebar/index.tsx
@@ -22,7 +22,7 @@ const Sidebar:React.FC<SideBarProps> = ({artwork, styles}) => {
     const prevButton = () => {
       if (prevArt != null){
         return (
-          <Link href={prevArt.key} className="flex items-center px-5 py-2 bg-lime text-white text-xs rounded-full">
+          <Link href={prevArt.key} className="flex items-center px-5 py-2 bg-lime text-white text-sm rounded-full">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="size-4 pr-1">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
         </svg>
@@ -33,7 +33,7 @@ const Sidebar:React.FC<SideBarProps> = ({artwork, styles}) => {
     const nextButton = () => {
       if (nextArt != null){
         return (
-          <Link href={nextArt.key} className="flex items-center px-5 py-2 bg-lime text-white text-xs rounded-full" >{nextArt.title}  
+          <Link href={nextArt.key} className="flex items-center px-5 py-2 bg-lime text-white text-sm rounded-full" >{nextArt.title}  
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="size-4 pl-1">
           <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
@@ -46,19 +46,19 @@ const Sidebar:React.FC<SideBarProps> = ({artwork, styles}) => {
       <Navigation />
       <div className="sm:pt-6">
         </div>
-      <div className="px-20 pt-2 text-lime uppercase font-tomarik sm:text-3xl md:text-4xl lg:text-6xl">
+      <div className="px-20 pt-2 text-lime uppercase font-tomarik text-4xl md:text-5xl lg:text-6xl">
         {artwork.title}
       </div>
-      <div className="px-20 tracking-24 font-tomarik text-offblack text-xs sm:text-base md:text-xl lg:text-2xl">
+      <div className="px-20 tracking-24 font-tomarik text-offblack text-xl md:text-lg lg:text-xl">
         {artwork.subtitle}
       </div>
-      <ul className="pt-4 px-20 py-2 uppercase text-bold text-xs text-green tracking-18 font-sans flex flex-row flex-wrap gap-x-2 gap-y-3">
+      <ul className="pt-4 px-20 py-2 uppercase text-bold text-green tracking-18 font-sans flex flex-row flex-wrap gap-x-2 gap-y-3 text-sm">
         {Tags()}
       </ul>
-      <div className="px-20 text-left pt-4 text-offblack font-sans tracking-5 text-xs sm:text-sm md:text-sm text-pretty">
+      <div className="px-20 text-left pt-4 text-offblack font-sans tracking-5 text-sm text-pretty">
         {artwork.text}
       </div>
-      <div className="flex justify-between py-16 px-20 text-left pt-8 text-green font-sans tracking-5 text-xs sm:text-sm md:text-sm">
+      <div className="flex justify-between py-16 px-20 text-left pt-8 text-green font-sans tracking-5 text-sm">
         {prevButton()}
         {nextButton()}
       </div>
